1ee1929cece6fdf3d7c487bf31536c97760e1893
Robert Winkler <rob121618@gmail.com>
2015-12-04 16:37:52 -0700

    Finished simulate

--- simulate_pysdl2.py
+++ simulate_pysdl2.py
@@ -52,13 +52,15 @@ GREENRECT  = (XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, YMARGIN + BUTTONSIZE + BUTTO
 def main():
 	#cause I don't want to pass these around
 	global ren, sprite_factory, sprite_renderer
-	global reset_button, new_button, solve_button
+	global clickedButton, BEEP1, BEEP2, BEEP3, BEEP4
 
 	#this only initializes video subsystem
 	sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO|sdl2.SDL_INIT_AUDIO)
 
-	window = sdl2.ext.Window("Slide Puzzle", size=(WINDOWWIDTH, WINDOWHEIGHT))
+	window = sdl2.ext.Window("Simulate", size=(WINDOWWIDTH, WINDOWHEIGHT))
 	ren = sdl2.ext.Renderer(window)
+	ren.blendmode = sdl2.SDL_BLENDMODE_BLEND
+	
 	window.show()
 
 	font_file = sysfont.get_font("freesans", sysfont.STYLE_BOLD)
@@ -70,9 +72,6 @@ def main():
 
 
 
-	#I should have to initialize sdl's audio subsystem before I do this
-	#sdl2.SDL_Init(sdl2.SDL_INIT_AUDIO) # or
-	#sdl2.SDL_InitSubSystem(sdl2.SDL_INIT_AUDIO)
 	sdlmixer.Mix_Init(sdlmixer.MIX_INIT_OGG)
 	sdlmixer.Mix_OpenAudio(44100, sdlmixer.MIX_DEFAULT_FORMAT, 2, 1024)
 	BEEP1 = sdlmixer.Mix_LoadWAV(b"beep1.ogg")
@@ -90,51 +89,103 @@ def main():
 	# when False, the pattern is playing. when True, waiting for the player to click a colored button:
 	waitingForInput = False
 
+	#directions text sprite
+	info_text = make_text(sprite_factory, "Match the pattern by clicking on the button or using the Q, W, A, S keys.", 10, WINDOWHEIGHT-25)
 
-	running = True
-	while running:
-		clickedButton = None
-		ren.clear(bgColor)
+	clickedButton = []
+	while True:
+		ren.fill((0, 0, WINDOWWIDTH, WINDOWHEIGHT), bgColor)
 		drawButtons()
 
 
-		ren.present()
+		score_text = make_text(sprite_factory, "Score: "+str(score), WINDOWWIDTH - 100, 10)
+		sprite_renderer.render(score_text)
+		sprite_renderer.render(info_text)
 
-		checkForQuit()
-		for event in sdl2.ext.get_events():
-			if event.type == sdl2.SDL_KEYDOWN:
-				sym = event.key.keysym.sym
-				if sym == sdl2.SDLK_q:
-					clickedButton = YELLOW
-				elif sym == sdl2.SDLK_w:
-					clickedButton = BLUE
-				elif sym == sdl2.SDLK_a:
-					clickedButton = RED
-				elif sym == sdl2.SDLK_s:
-					clickedButton = GREEN
-	
+		handle_events()
+
+		if not waitingForInput:
+			# play the pattern
+			ren.present()
+			sdl2.SDL_Delay(1000)
+			pattern.append(random.choice((YELLOW, BLUE, RED, GREEN)))
+			for button in pattern:
+				handle_events()
+				flashButtonAnimation(button)
+				sdl2.SDL_Delay(FLASHDELAY)
+			waitingForInput = True
+		else:
+			# wait for the player to enter buttons
+			if clickedButton and clickedButton[0] == pattern[currentStep]:
+				# pushed the correct button
+				flashButtonAnimation(clickedButton[0])
+				currentStep += 1
+				lastClickTime = sdl2.SDL_GetTicks()
+				
+				#could replace with collections.deque but premature optimizations and all that
+				clickedButton.pop(0)
+
+				if currentStep == len(pattern):
+					# pushed the last button in the pattern
+					changeBackgroundAnimation()
+					score += 1
+					waitingForInput = False
+					currentStep = 0 # reset back to first step
+					clickedButton.clear()
+
+			elif (clickedButton and clickedButton[0] != pattern[currentStep]) or (currentStep != 0 and sdl2.SDL_GetTicks() - TIMEOUT*1000 > lastClickTime):
+				# pushed the incorrect button, or has timed out
+				gameOverAnimation()
+				# reset the variables for a new game:
+				pattern = []
+				clickedButton.clear()
+				currentStep = 0
+				waitingForInput = False
+				score = 0
+				sdl2.SDL_Delay(1000)
+				changeBackgroundAnimation()
+
+		ren.present()
+		sdl2.SDL_Delay(FPS)
+
 	
 	shutdown()
 
 
-#TODO expand for ESC
-def checkForQuit():
-	eventarray = (sdl2.SDL_Event * 50)()
-	numevents = sdl2.SDL_PeepEvents(eventarray, 50, sdl2.SDL_PEEKEVENT, sdl2.SDL_FIRSTEVENT, sdl2.SDL_LASTEVENT)
-	for i in range(numevents):
-		if eventarray[i].type == sdl2.SDL_QUIT:
+#will ignore clicks while busy doing animation etc.
+#ie you can't click faster than it can flash them
+def handle_events():
+	global clickedButton
+	for event in sdl2.ext.get_events():
+		if event.type == sdl2.SDL_QUIT:
 			shutdown()
-		elif eventarray[i].type == sdl2.SDL_KEYDOWN:
-			sym = eventarray[i].key.keysym.sym
+		elif event.type == sdl2.SDL_MOUSEBUTTONUP:
+			mousex, mousey = event.button.x, event.button.y
+			clickedButton.append(getButtonClicked(mousex, mousey))
+		elif event.type == sdl2.SDL_KEYDOWN:
+			sym = event.key.keysym.sym
 			if sym == sdl2.SDLK_ESCAPE:
 				shutdown()
+			elif sym == sdl2.SDLK_q:
+				clickedButton.append(YELLOW)
+			elif sym == sdl2.SDLK_w:
+				clickedButton.append(BLUE)
+			elif sym == sdl2.SDLK_a:
+				clickedButton.append(RED)
+			elif sym == sdl2.SDLK_s:
+				clickedButton.append(GREEN)
+	
 
+def make_text(sprite_factory, text, top, left):
+	button = sprite_factory.from_text(text)
+	button.position = top, left
+	return button
 
 
 def shutdown():
 	sdlmixer.Mix_CloseAudio()
-	sdl2.SDL_Quit()
 	#sdl2.ext.quit()
+	sdl2.SDL_Quit()
 	sys.exit()
 	#do I need to do anything else here?
 
@@ -146,6 +197,102 @@ def drawButtons():
 	ren.fill(GREENRECT, GREEN)
 
 
+def flashButtonAnimation(color, animationSpeed=50):
+	if color == YELLOW:
+		sound = BEEP1
+		flashColor = BRIGHTYELLOW
+		rectangle = YELLOWRECT
+	elif color == BLUE:
+		sound = BEEP2
+		flashColor = BRIGHTBLUE
+		rectangle = BLUERECT
+	elif color == RED:
+		sound = BEEP3
+		flashColor = BRIGHTRED
+		rectangle = REDRECT
+	elif color == GREEN:
+		sound = BEEP4
+		flashColor = BRIGHTGREEN
+		rectangle = GREENRECT
+
+	r, g, b, a = flashColor
+
+	channel = sdlmixer.Mix_PlayChannel(-1, sound, 0)
+	for start, end, step in ((0, 255, 1), (255, 0, -1)): # animation loop
+		for alpha in range(start, end, animationSpeed * step):
+			handle_events()
+			ren.fill(rectangle, color)
+			ren.fill(rectangle, (r,g,b,alpha))
+			ren.present()
+			sdl2.SDL_Delay(FPS)
+
+	ren.fill(rectangle, color)
+	ren.present()
+
+def hit_rect(rect, pt):
+	"""returns True if pt is inside rect (rect is tuple (x, y, w, h))"""
+	if pt[0] >= rect[0] and pt[0] <= rect[0] + rect[2] and pt[1] >= rect[1] and pt[1] <= rect[1]+rect[3]:
+		return True
+	return False
+
+def hit_bbox(box, pt):
+	"""returns True if pt is inside box (box is tuple (x1, y1, x2, y2))"""
+	if pt[0] >= box[0] and pt[0] <= box[2] and pt[1] >= box[1] and pt[1] <= box[3]:
+		return True
+	return False
+
+def getButtonClicked(x, y):
+	if hit_rect(YELLOWRECT, (x, y)):
+		return YELLOW
+	elif hit_rect(BLUERECT, (x, y)):
+		return BLUE
+	elif hit_rect(REDRECT, (x, y)):
+		return RED
+	elif hit_rect(GREENRECT, (x, y)):
+		return GREEN
+	return None
+
+
+def changeBackgroundAnimation(animationSpeed=40):
+	global bgColor
+	newBgColor = sdl2.ext.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+	r, g, b, a = newBgColor
+	for alpha in range(0, 255, animationSpeed): # animation loop
+		handle_events()
+		ren.fill((0, 0, WINDOWWIDTH, WINDOWHEIGHT), (r,g,b,alpha))
+		
+		drawButtons() # redraw the buttons on top of the tint
+
+		ren.present();
+		sdl2.SDL_Delay(FPS)
+
+	bgColor = newBgColor
+
+
+def gameOverAnimation(color=WHITE, animationSpeed=50):
+	# play all beeps at once, then flash the background
+	
+	# play all four beeps at the same time, roughly.
+	channel = sdlmixer.Mix_PlayChannel(-1, BEEP1, 0)
+	channel = sdlmixer.Mix_PlayChannel(-1, BEEP2, 0)
+	channel = sdlmixer.Mix_PlayChannel(-1, BEEP3, 0)
+	channel = sdlmixer.Mix_PlayChannel(-1, BEEP4, 0)
+
+	r, g, b, a = color
+	for i in range(3): # do the flash 3 times
+		for start, end, step in ((0, 255, 1), (255, 0, -1)):
+			# The first iteration in this loop sets the following for loop
+			# to go from 0 to 255, the second from 255 to 0.
+			for alpha in range(start, end, animationSpeed * step): # animation loop
+				# alpha means transparency. 255 is opaque, 0 is invisible
+				handle_events()
+
+				ren.fill((0, 0, WINDOWWIDTH, WINDOWHEIGHT), (r,g,b,alpha))
+				drawButtons()
+
+				ren.present()
+				sdl2.SDL_Delay(FPS)
 
 
 
